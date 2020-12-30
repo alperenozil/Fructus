623b20d49bdd87fba456525673adf16c6df763bb
alperenozil <necdet.ozil@hacettepe.edu.tr>
2020-12-30 10:01:38 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Necdet Alperen Özil on 30.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
