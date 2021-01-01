bb889744e90e6ef77943400a1ec9b08e213936cc
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-01-01 10:30:11 +0300

    Create StartButtonView.swift

---
+++ Fructus/View/StartButtonView.swift
@@ -0,0 +1,24 @@
+//
+//  StartButtonView.swift
+//  Fructus
+//
+//  Created by Necdet Alperen Özil on 1.01.2021.
+//
+
+import SwiftUI
+    // PROPERTIES
+
+    // BODY
+struct StartButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+    
+    // PREVIEW
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+            .previewLayout(.sizeThatFits)
+    }
+}
