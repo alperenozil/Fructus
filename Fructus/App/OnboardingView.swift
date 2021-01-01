b7857d0492de54c34aaa67bb544438366c4788fc
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-01-01 13:11:23 +0300

    Update OnboardingView.swift

---
+++ Fructus/App/OnboardingView.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardingView.swift
+//  Fructus
+//
+//  Created by Necdet Alperen Özil on 1.01.2021.
+//
+
+import SwiftUI
+    // PROPERTIES
+struct OnboardingView: View {
+    
+    // BODY
+    var body: some View {
+        TabView {
+            ForEach(0..<5){ item in
+                FruitCardView()
+            }
+        }
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical,20)
+    }
+}
+    
+    // PREVIEW
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
