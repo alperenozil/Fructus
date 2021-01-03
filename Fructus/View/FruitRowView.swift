898255ed6161a749682aab4333879c21fd52ce49
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-01-03 19:55:51 +0300

    Create FruitRowView.swift

---
+++ Fructus/View/FruitRowView.swift
@@ -0,0 +1,35 @@
+//
+//  FruitRowView.swift
+//  Fructus
+//
+//  Created by Necdet Alperen Özil on 3.01.2021.
+//
+
+import SwiftUI
+
+struct FruitRowView: View {
+    var fruit: Fruit
+    var body: some View {
+        HStack {
+            Image(fruit.image).renderingMode(.original).resizable().scaledToFit().frame(width: 80, height: 80, alignment: .center)
+                .background(LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .top, endPoint: .bottom))
+                .cornerRadius(8)
+            VStack(alignment: .leading, spacing: 5)
+                {
+                Text(fruit.title)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                Text(fruit.headline).font(.caption)
+                    .foregroundColor(Color.secondary)
+            }
+        }// endof HSTACK
+    }
+}
+
+struct FruitRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitRowView(fruit: fruitsData[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
